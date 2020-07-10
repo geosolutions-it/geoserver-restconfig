462b045d76e89382951ec11f6fa1ea15b9ca4f5a
afabiani <alessio.fabiani@geo-solutions.it>
2020-07-10 10:55:51 +0200

    - Bump geoserver-restconfig to version 2.0.4.5

--- src/geoserver/catalog.py
+++ src/geoserver/catalog.py
@@ -1130,14 +1130,14 @@ class Catalog(object):
                 )
         return all_styles
 
-    def get_style(self, name, workspace=None):
+    def get_style(self, name, workspace=None, recursive=False):
         '''
           returns a single style object.
           Will return None if no style is found.
           Will raise an error if more than one style with the same name is found.
         '''
 
-        styles = self.get_styles(names=name, workspaces=[workspace])
+        styles = self.get_styles(names=name, workspaces=[workspace], recursive=recursive)
         return self._return_first_item(styles)
 
     def create_style(self, name, data, overwrite=False, workspace=None, style_format="sld10", raw=False):
